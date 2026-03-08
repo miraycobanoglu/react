bda84769963238d04fd462aacf7c86dc8dd180b4
Miray Çobanoğlu <miraycobanogluu@gmail.com>
2026-03-09 02:20:35 +0300

    header görünüm

--- uygulama/src/components/Header.jsx
+++ uygulama/src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { MdPostAdd } from 'react-icons/md'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex items-center justify-between bg-indigo-600 text-white px-4 py-3">
+      <div className="text-3xl"> REACT UYGULAMA </div>
+      <div className="flex items-center gap-4">
+        <div>
+            <select className="cursor-pointer" name="" id="">
+                <option value="asc">artan</option>
+                <option value="desc">azalan</option>
+            </select>
+        </div>
+        <div>
+            <input className="h-10 px-2 border border-gray-300 rounded" type="text" placeholder='arama yapınız...' />
+        </div>
+        <div className="bg-indigo-800 w-10 h-10 rounded-full flex items-center justify-center cursor-pointer">
+            <MdPostAdd/>
+        </div>
+      </div>
+    </div>
   )
 }
 

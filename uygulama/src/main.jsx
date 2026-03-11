80f771b7699ca0e43d86e661d7a9079a4111ffb3
Miray Çobanoğlu <miraycobanogluu@gmail.com>
2026-03-11 03:17:09 +0300

    modal eklendi store düzeltildi

--- uygulama/src/main.jsx
+++ uygulama/src/main.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
-createRoot(document.getElementById('root')).render(
-  <>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
     <App />
-  </>
+    </Provider>
+  </StrictMode>
 )
